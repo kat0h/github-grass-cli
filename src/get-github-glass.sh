92bdca1ae0330891c0ea8aae305fc22dc49c2f04
KotaKato <peony.btn@gmail.com>
2020-12-13 00:30:46 +0900

    Fix Bug コマンドライン引数が無視されるミスを修正

--- src/get-github-glass.sh
+++ src/get-github-glass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OLDIFS=$IFS
 IFS='\n'
-response=($(curl -Lso- "https://github.com/kato-k" | grep "var(--color-calendar-graph" | grep "data-date" | awk '{print $9,$7}' | sed "s/fill=\"var(--color-calendar-graph-day-//" | sed "s/-bg)\"//" | sed "s/data-date=\"//" | sed "s/\"\/\>//" | sed "s/)\"//" | sed "s/L//" | sed "s/bg/0/"))
+response=($(curl -Lso- "https://github.com/$1" | grep "var(--color-calendar-graph" | grep "data-date" | awk '{print $9,$7}' | sed "s/fill=\"var(--color-calendar-graph-day-//" | sed "s/-bg)\"//" | sed "s/data-date=\"//" | sed "s/\"\/\>//" | sed "s/)\"//" | sed "s/L//" | sed "s/bg/0/"))
 echo $response
 IFS=$OLDIFS
