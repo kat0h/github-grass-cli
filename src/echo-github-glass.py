e7fc655c06e2d283db895339b6ce1a8974739ef2
Kota Kato <peony.btn@gmail.com>
2020-12-16 17:17:10 +0900

    fix issue #1

--- src/echo-github-glass.py
+++ src/echo-github-glass.py
@@ -1,6 +1,8 @@
+import sys
+
 glass = []
-for i in range(53*7):
-    j = input().split()
+for i in sys.stdin:
+    j = i.split()
     glass.append([j[0], int(j[1])])
 color = [
     "\033[30m\033[48;2;230;232;237m⬜︎\033[0m",
@@ -9,7 +11,10 @@ color = [
     "\033[30m\033[48;2;40;146;60m⬜︎\033[0m",
     "\033[30m\033[48;2;26;90;41m⬜︎\033[0m"
 ]
-for i in range(7):
-    for j in range(53):
-        print(color[glass[i+j*7][1]], end='')
+height = 7
+width = len(glass)/7 if len(glass) % 7 == 0 else len(glass)//7+1
+for i in range(height):
+    for j in range(width):
+        if len(glass) > i+j*7:
+            print(color[glass[i+j*7][1]], end='')
     print("")
