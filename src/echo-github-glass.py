8b1fbd5e8d0d7518bdb49a98fbea473826d38ddd
Kota Kato <45391880+kato-k@users.noreply.github.com>
2020-12-23 23:03:22 +0900

    fix typo

--- src/echo-github-glass.py
+++ src/echo-github-glass.py
@@ -1,9 +1,9 @@
 import sys
 
-glass = []
+grass = []
 for i in sys.stdin:
     j = i.split()
-    glass.append([j[0], int(j[1])])
+    grass.append([j[0], int(j[1])])
 color = [
     "\033[30m\033[48;2;230;232;237m⬜︎\033[0m",
     "\033[30m\033[48;2;140;231;152m⬜︎\033[0m",
@@ -12,10 +12,10 @@ color = [
     "\033[30m\033[48;2;26;90;41m⬜︎\033[0m"
 ]
 height = 7
-width = int(len(glass)/7) if len(glass) % 7 == 0 else int(len(glass)//7+1)
+width = int(len(grass)/7) if len(grass) % 7 == 0 else int(len(grass)//7+1)
 print(width)
 for i in range(height):
     for j in range(width):
-        if len(glass) > i+j*7:
-            print(color[glass[i+j*7][1]], end='')
+        if len(grass) > i+j*7:
+            print(color[grass[i+j*7][1]], end='')
     print("")
